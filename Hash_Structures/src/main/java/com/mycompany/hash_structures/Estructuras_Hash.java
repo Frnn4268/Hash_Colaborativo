a4cadff28559dfd3c9844d0362686f38dcc19602
USUARIO <appstutorialesy@gmail.comm>
2022-03-26 12:35:17 -0600

    Add sout

--- Hash_Structures/src/main/java/com/mycompany/hash_structures/Estructuras_Hash.java
+++ Hash_Structures/src/main/java/com/mycompany/hash_structures/Estructuras_Hash.java
@@ -3,5 +3,6 @@ package com.mycompany.hash_structures;
 public class Estructuras_Hash {
     public static void main(String[] args) {
         System.out.println("Hola");
+        System.out.println("Hola2");
     }
 }
