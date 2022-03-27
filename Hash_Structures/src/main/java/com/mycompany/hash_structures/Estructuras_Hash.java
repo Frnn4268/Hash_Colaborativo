657d84ce5c1ff9e640663757d444c068965c28f3
USUARIO <appstutorialesy@gmail.comm>
2022-03-26 19:39:17 -0600

    Se modificaron funciones

--- Hash_Structures/src/main/java/com/mycompany/hash_structures/Estructuras_Hash.java
+++ Hash_Structures/src/main/java/com/mycompany/hash_structures/Estructuras_Hash.java
@@ -2,7 +2,7 @@ package com.mycompany.hash_structures;
 
 public class Estructuras_Hash {
     public static void main(String[] args) {
-         Registro c = new Registro();
+        Registro1 c = new Registro1();
         c.setLocationRelativeTo(null);
         c.setVisible(true);
     }
